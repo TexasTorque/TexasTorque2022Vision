61f4f1dddaec4b91677414918510aa0ce91a4bb2
realSaddy <jrp30074@gmail.com>
2022-01-22 23:00:30 -0600

    Jack algo for ball detection -- certified fresh

---
+++ jackalgo/CMakeFiles/TorqueVision.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/saddy/Projects/TorqueVision2022/src/main.cpp" "CMakeFiles/TorqueVision.dir/src/main.cpp.o" "gcc" "CMakeFiles/TorqueVision.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
