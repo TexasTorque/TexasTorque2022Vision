e9e14f89ef0f3046e92a2cdc6c65af786456a43e
realSaddy <jrp30074@gmail.com>
2022-01-25 12:22:54 -0600

    Lots of improvements

--- src/main.cc
+++ src/main.cc
@@ -48,28 +48,22 @@ public:
 //     Value:   "blue"
 //     Value:   "red"
 //     Default: "none"
-//   Entry:     ball_horizontal_position
+//   Entry:     x
 //     Type:    double
-//     Value:   -1/2w <= x <= 1/2w
+//     Value:   0 <= x <= 640
 //     Default: 0.0
-//   Entry: 	frames_per_second
+//   Entry: 	r
 //     Type:    double
-//     Value: 	0 <= x
-//     Default: -1.0
+//     Value: 	0 <= 640
+//     Default: 0.0
 
 NetworkTablePointer initializeNetworkTable(std::string identifier) {
-	//return nt::NetworkTableInstance::GetDefault().GetTable(identifier);
 	auto instance = nt::NetworkTableInstance::GetDefault();
 	instance.StartClientTeam(1477);
-	//instance.StartDSClient();
-	while (!instance.IsConnected());
-		//std::printf("NOT CONNECTED\n");
 	return instance.GetTable(identifier);
 }
 
-Bound fetchDetectionBounds(NetworkTablePointer tablePointer) {
-    std::string color = tablePointer->GetEntry("alliance_color").GetString("none");
-
+Bound fetchDetectionBounds(NetworkTablePointer tablePointer, std::string color) {
     if (color == "none") throw std::runtime_error("Alliance color could not read from network tables");
     else if (color == "red") return Bound(RED);
     else if (color == "blue") return Bound(BLUE);
@@ -93,72 +87,72 @@ void checkForFrameEmpty(cv::Mat frame) {
      }
  	return biggest;
  }
-//
-//cv::Vec3f fetchBiggestCircle(std::vector<cv::Vec3f> circles) {
-//	return *std::max_element(circles.begin(), circles.end(),
-//			[](const cv::Vec3f& a, const cv::Vec3f& b) { // this is a C++ lambda - >25% /g/ approved!
-//		return a[2] < b[2];
-//	});
-//}
 
-void processCenter(cv::Mat* input, Bound bounds, nt::NetworkTableEntry xEntry) {
+void processCenter(cv::Mat* input, Bound bounds, nt::NetworkTableEntry* xEntry, nt::NetworkTableEntry* rEntry) {
     if (bounds.color == RED) *input = ~*input; // invert color space to allow const
+    // convert to HSV color space
     cvtColor(*input, *input, cv::COLOR_BGR2HSV);
+    // remove non-colored range
     inRange(*input, bounds.lower, bounds.upper, *input);
 
+    // blur together features
     medianBlur(*input, *input, 17);
+
+    // erode extraneous data
     erode(*input, *input, cv::Mat(), cv::Point(-1, -1), 3);
+
+    // dilate good data
     dilate(*input, *input, cv::Mat(), cv::Point(-1, -1), 5);
+
+    // Remove small details
     morphologyEx(*input, *input, cv::MORPH_HITMISS, cv::Mat());
 
+    // get circles using hough-circles
     std::vector<cv::Vec3f> circles;
     HoughCircles(*input, circles, cv::HOUGH_GRADIENT, 1, 25, 30, 15, 50, 0);
 
+    // if there is a circle
     if(circles.size() > 0) {
+        //get the largest one and send
         cv::Vec3f biggest = fetchBiggestCircle(circles);
 
         cv::Point center = cv::Point(biggest[0], biggest[1]);
-        // circle center
+
+        // draw circle center
         cv::circle(*input, center, 1, cv::Scalar(0, 100, 100), 3, cv::LINE_AA);
-        // set xEntry, unfinished but have to switch class.
+
+        // set x-value (px) and radius (px)
+        xEntry->SetDouble(biggest[0]);
+        rEntry->SetDouble(biggest[2]);
+    } else {
+        // if none are found, set default value (0)
+        xEntry->SetDouble(0);
+        rEntry->SetDouble(0);
     }
-    // circle outline
-	//int radius = biggest[2];
-	//circle(*input, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
-    //
-	//cv::Moments m = cv::moments(*input);
-	//double hu[7];
-	//cv::HuMoments(m, hu);
-	//int cX = m.m10 / m.m00;
-	//int cY = m.m01 / m.m00;
-	//cv::circle(*input, cv::Point(cX, cY), 5, (255, 255, 255), -1);
 }
 
 int main(int argc, char** argv) {
     NetworkTablePointer programTablePointer = initializeNetworkTable("ball_detection");
 
-    Bound detectionBounds = fetchDetectionBounds(programTablePointer);
+    std::string color = programTablePointer->GetEntry("alliance_color").GetString("none");
 
-    frc::CameraServer* cameraServer = frc::CameraServer::GetInstance();
-    cs::CvSink cvSink = cameraServer->GetVideo();
-    cs::CvSource cvSource = cameraServer->PutVideo("USB Cam", 640, 480);
+    Bound detectionBounds = fetchDetectionBounds(programTablePointer, color);
+
+    cs::CvSink cvSink = frc::CameraServer::GetInstance()->GetVideo();
+    cs::CvSource cvSource = frc::CameraServer::GetInstance()->PutVideo("VP front", 640, 480);
 	
     cv::Mat frame;
 
     nt::NetworkTableEntry ballEntryX = programTablePointer->GetEntry("x");
     nt::NetworkTableEntry ballEntryR = programTablePointer->GetEntry("r");
-    nt::NetworkTableEntry fpsEntry = programTablePointer->GetEntry("frames_per_second");
-	
+
+
     // Initialize program loop while reading
-
-    std::string color = programTablePointer->GetEntry("alliance_color").GetString("none");
-
     while (true) {
-            long c = cvSink.GrabFrame(frame);
-            if(c == 0) continue;
-
-            cv::Point center = processCenter(&frame, detectionBounds);
-            ballEntryX.SetDouble(center.x);
+            if(cvSink.GrabFrame(frame) == 0) {
+                cvSource.NotifyError(cvSink.GetError());
+            }
+            processCenter(&frame, detectionBounds, &ballEntryX, &ballEntryR);
 
             cvSource.PutFrame(frame);
     }
